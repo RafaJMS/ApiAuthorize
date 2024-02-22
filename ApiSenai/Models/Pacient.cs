165228c9c3d10cd62c815a1c59946d20649dc63c
Rafael Mecenas <rafaeljmecenas1@gmail.com>
2024-02-22 17:09:11 -0300

    Adicionar arquivos de projeto.

---
+++ ApiSenai/Models/Pacient.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiSenai.Models
+{
+    public class Pacient
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+    }
+}
